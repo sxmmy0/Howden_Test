9a106a8279de2ede11926f6c6b39c275b21377f1
Samuel Obembe <samuel.obembe97@gmail.com>
2025-05-22 02:28:40 +0100

    Updated App.js to keep user logged in

--- howden-job-queue/src/App.js
+++ howden-job-queue/src/App.js
@@ -1,10 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import JobsPage from "./pages/JobsPage";
 
 function App() {
-  const [userId, setUserId] = useState(null);
+  const [userId, setUserId] = useState(() => {
+    return localStorage.getItem("userId");
+  });
+
+  useEffect(() => {
+    if (userId) {
+      localStorage.setItem("userId", userId);
+    } else {
+      localStorage.removeItem("userId");
+    }
+  }, [userId]);
 
   return (
     <Router>
